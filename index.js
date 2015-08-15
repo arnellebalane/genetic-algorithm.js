f06c21881588d7ccab5e3d1ab69f18831f07ac78
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-08-15 22:43:22 +0800

    updated main file to accept config

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function GeneticAlgorithm() {
-    console.log('GeneticAlgorithm');
+import utils from './utils';
+
+
+function GeneticAlgorithm(config) {
+    this.generatePopulation = config.generatePopulation || utils.noop;
+    this.calculateFitness = config.generatePopulation || utils.noop;
+    this.recombinationProcess = config.recombinationProcess || utils.noop;
+    this.mutationProcess = config.mutationProcess || utils.noop;
+    this.maxGenerations = config.maxEpochs || 1000;
+    this.perfectFitness = config.perfectFitness || 0;
 }
 
 
