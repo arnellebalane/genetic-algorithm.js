b36fc0bbf74706d1c916029af685e1898b656197
Arnelle Balane <arnellebalane@gmail.com>
2015-08-17 00:20:19 +0800

    main class properties checking

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import 'babel/polyfill';
-import { extend } from './utils';
+import { extend, items } from './utils';
+
+
+var properties = {
+    individual: { required: true, type: 'function' },
+    recombine: { required: true, type: 'function' },
+    mutate: { required: true, type: 'function' },
+    fitness: { required: true, type: 'function' },
+    deconstruct: { required: true, type: 'function' },
+    reconstruct: { required: true, type: 'function' },
+    perfectFitness: { required: true }
+};
 
 
 class GeneticAlgorithm {
@@ -7,6 +18,15 @@ class GeneticAlgorithm {
         this.populationSize = 10;
         this.maxGenerations = 1000;
         extend(this, options);
+
+        for (let [key, value] of items(properties)) {
+            if (value.required && !this.hasOwnProperty(key)) {
+                throw new Error(`Property "${key}" is required.`);
+            } else if (value.type && typeof this[key] !== value.type) {
+                throw new Error(`Property "${key}" should be of type
+                    "${value.type}".`);
+            }
+        }
     }
 }
 
