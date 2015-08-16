abe2cbf3e9586c10ceb6cab33f3bb0c694eb1100
Arnelle Balane <arnellebalane@gmail.com>
2015-08-16 13:53:35 +0800

    added extend method to utils

--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
-exports.noop = function() {};
+function noop() {}
+
+
+function extend(base, extension) {
+    return { ...base, ...extension };
+}
+
+
+exports.noop = noop;
+exports.extend = extend;
