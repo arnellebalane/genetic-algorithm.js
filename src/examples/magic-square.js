06803f012a149bf64c5814932acac533fc28561f
Arnelle Balane <arnellebalane@gmail.com>
2015-08-22 04:45:08 +0800

    added magic square sample problem

---
+++ src/examples/magic-square.js
@@ -0,0 +1,51 @@
+import GeneticAlgorithm from '../index';
+import { items } from '../utils';
+
+
+var magic = new GeneticAlgorithm({
+    individual: () => {
+        var individual = [];
+        for (let i = 0; i < 3; i++) {
+            individual.push([random(), random(), random()]);
+        }
+        return individual;
+    },
+    recombine: () => {},
+    mutate: () => {},
+    fitness: (individual) => {
+        var sums = [], left = 0, right = 0;
+        for (let i = 0, l = individual.length; i < l; i++) {
+            sums.push(sum(individual[i]));
+            sums.push(sum(individual.map(row => row[i])));
+            left += individual[i][i];
+            right += individual[i][l - i - 1];
+        }
+        sums.push(left, right);
+        var counts = sums.reduce((counts, sum) => {
+            counts[sum] = counts.hasOwnProperty(sum) ? counts[sum] + 1 : 1;
+            return counts;
+        }, {});
+        var highest = Object.keys(counts).map(count => counts[count])
+            .reduce((highest, count) => count > highest ? count : highest, 0);
+        return sums.length - highest;
+    },
+    deconstruct: () => {},
+    reconstruct: () => {},
+    perfectFitness: 0
+});
+
+
+magic.run();
+
+
+
+
+
+function random() {
+    return Math.ceil(Math.random() * 9);
+}
+
+
+function sum(array) {
+    return array.reduce((sum, n) => sum + n, 0);
+}
