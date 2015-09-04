04924a7f86f3e7309e584435a6277d5e13fff8f5
Julia Menchavez <jedijulia@users.noreply.github.com>
2015-09-04 23:30:46 +0800

    added mutation class

---
+++ src/core/mutation.js
@@ -0,0 +1,9 @@
+import 'babel/polyfill';
+import { NotImplementedError } from './errors';
+
+
+export default class Mutation {
+    mutate(individual) {
+        throw new NotImplementedError(`${this.constructor.name}.mutate`);
+    }
+}
