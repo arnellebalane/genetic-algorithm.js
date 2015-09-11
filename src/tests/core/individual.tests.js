35a2e9381fc90a72e549cbc847c025ddf51e627a
Arnelle Balane <arnellebalane@gmail.com>
2015-09-11 21:28:50 +0800

    updated tests for individual to consider changes in individual base class

--- src/tests/core/individual.tests.js
+++ src/tests/core/individual.tests.js
@@ -1,22 +1,32 @@
 import 'babel/polyfill';
 import expect from 'expect.js';
 import sinon from 'sinon';
+import Allele from '../../core/allele';
 import Individual from '../../core/individual';
 import utils from '../../utils';
+
+
+function createIndividual() {
+    var alleles = [];
+    for (let i = 0; i < 3; i++) {
+        alleles.push(new Allele(i + 1));
+    }
+    return new Individual(alleles, [1, 2, 3, 4, 5]);
+}
 
 
 describe('Individual', () => {
     describe('#alleles', () => {
         it('getter should use the deconstruct method', () => {
-            var individual = new Individual([1, 2, 3], [1, 2, 3, 4, 5]);
+            var individual = createIndividual();
             var deconstruct = sinon.spy(individual, 'deconstruct');
             var alleles = individual.alleles;
             expect(deconstruct.called).to.be(true);
-            expect(alleles).to.eql([1, 2, 3]);
+            expect(alleles.map(allele => allele.value)).to.eql([1, 2, 3]);
         });
 
         it('setter should use the reconstruct method', () => {
-            var individual = new Individual([1, 2, 3], [1, 2, 3, 4, 5]);
+            var individual = createIndividual();
             var reconstruct = sinon.spy(individual, 'reconstruct');
             individual.alleles = [3, 4, 5];
             expect(reconstruct.called).to.be(true);
@@ -26,29 +36,28 @@ describe('Individual', () => {
 
     describe('deconstruct(alleles)', () => {
         it('should be called with the instance\'s current alleles', () => {
-            var individual = new Individual([1, 2, 3], [1, 2, 3, 4, 5]);
+            var individual = createIndividual();
             var deconstruct = sinon.spy(individual, 'deconstruct');
             var alleles = individual.alleles;
-            expect(deconstruct.calledWithExactly([1, 2, 3])).to.be(true);
-            expect(alleles).to.eql([1, 2, 3]);
+            expect(deconstruct.calledWithExactly(alleles)).to.be(true);
+            expect(alleles.map(allele => allele.value)).to.eql([1, 2, 3]);
         });
     });
 
     describe('allele()', () => {
-        it('should return an element of `possibleAlleles`', () => {
-            var individual = new Individual([1, 2, 3], [1, 2, 3, 4, 5]);
+        it('should return an allele contained in `possibleAlleles`', () => {
+            var individual = createIndividual();
             var allele = individual.allele();
-            expect([1, 2, 3, 4, 5]).to.contain(allele);
+            expect([1, 2, 3, 4, 5]).to.contain(allele.value);
         });
 
         it('should not return the same allele all the time', () => {
-            var possibleAlleles = [1, 2, 3, 4, 5];
-            var individual = new Individual([1, 2, 3], possibleAlleles);
+            var individual = createIndividual();
             var count = 0;
             var repetitions = 0;
             var allele = individual.allele();
             while (count++ < 10) {
-                if (individual.allele() === allele) {
+                if (individual.allele().value === allele.value) {
                     repetitions++;
                 }
             }
