e9e1e40c672d93e054dfe1e5b5220eb0623fd151
Arnelle Balane <arnellebalane@gmail.com>
2015-09-17 20:09:35 +0800

    added tournament parent selector

---
+++ src/core/parent-selectors/tournament.js
@@ -0,0 +1,35 @@
+import 'babel/polyfill';
+import ParentSelector from '../parent-selector';
+
+
+export default class TournamentParentSelector extends ParentSelector {
+    constructor(k) {
+        super();
+        this.k = k;
+    }
+
+    select(population) {
+        var parents = [];
+        while (parents.length !== population.length) {
+            let group = [];
+            while (group.length !== this.k) {
+                let index = Math.floor(Math.random() * population.length);
+                group.push(population[index]);
+            }
+            group = this.rank(group);
+            parents.push(group[0]);
+        }
+        return parents;
+    }
+
+    rank(population, order = 1) {
+        return [...population].sort((a, b) => {
+            if (a.fitness() < b.fitness()) {
+                return -1 * order;
+            } else if (b.fitness() < a.fitness()) {
+                return 1 * order;
+            }
+            return 0;
+        });
+    }
+}
